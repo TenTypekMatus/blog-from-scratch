607a5c7b306eced5e284c389f42847587d80b782
Matus Mastena <Shadiness9530@proton.me>
2023-03-09 12:24:45 +0100

    Big update

---
+++ gen-posts.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Set the directory where Markdown files are located
+md_dir="./posts"
+
+# Set the directory where the HTML files will be saved
+html_dir="./rendered"
+
+# Set the name of the index file
+index_file="index.html"
+
+# Delete the existing index file if it exists
+rm -f "${html_dir}/${index_file}"
+
+# Loop through each Markdown file in the directory
+for md_file in "${md_dir}"/*.md; do
+  # Convert the Markdown file to HTML
+  html_file="${html_dir}/$(basename "$md_file" .md).html"
+  pandoc -f markdown -t html "${md_file}" -o "${html_file}"
+
+  # Get the title of the Markdown file
+  title=$(grep -E '^# ' "${md_file}" | sed -e 's/^# //')
+
+  # Add the HTML file as an entry in the index file
+  echo "<li><a href=\"$(basename "$html_file")\">${title}</a></li>" >> "${html_dir}/temp.html"
+done
+
+# Add the list items to the body of the index file
+sed -i '/<body>/r '${html_dir}/temp.html'' "./${index_file}"
+rm -f "${html_dir}/temp.html"
--- posts/gen-posts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-src=$(ls | grep "*.md")
-for i in $src; do
-    echo "converting $i"
-    pandoc "$i".md -o "$i".html --parse-raw
-done
