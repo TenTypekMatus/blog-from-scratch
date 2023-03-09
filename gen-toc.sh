85ef170fc52ae1224cfd73c969d2ddb08ea8bbc2
Matus Mastena <Shadiness9530@proton.me>
2023-03-09 17:18:12 +0100

    Hell yeah!

--- gen-toc.sh
+++ gen-toc.sh
@@ -4,7 +4,7 @@
 input_file=$1
 
 # Get all of the headings from the input file
-headings=$(grep "^#" $input_file)
+headings=$(grep "^#" "$input_file")
 
 # Loop through the headings and print them out as a table of contents
 echo "Table of Contents:"
@@ -12,13 +12,13 @@ echo ""
 
 while read -r line; do
     # Get the level of the heading (i.e. the number of # characters)
-    level=$(echo $line | awk '{print gsub(/#/, "")}')
+    level=$(echo "$line" | awk '{print gsub(/#/, "")}')
     
     # Get the text of the heading (without the # characters)
-    text=$(echo $line | sed -e 's/^#\+//' -e 's/^[[:space:]]*//')
+    text=$(echo "$line" | sed -e 's/^#\+//' -e 's/^[[:space:]]*//')
     
     # Create a string of spaces to indent the heading based on its level
-    indent=$(printf ' %.0s' $(seq 1 $level))
+    indent=$(printf ' %.0s' $(seq 1 "$level"))
     
     # Print out the heading with the appropriate indentation
     echo "${indent}- [${text}](${input_file}#${text})"
