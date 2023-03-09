3925e83e7ad60e05e956be90b3ad4477b03e1f76
Matus Mastena <Shadiness9530@proton.me>
2023-03-09 15:26:51 +0100

    Made Readme.

---
+++ README.md
@@ -0,0 +1,8 @@
+# JS-free blogging software aka blog from scratch
+What I used:
+- Pandoc
+- Plain HTML and CSS
+# How it works
+1. Make a new file in the `posts` directory
+2. Run ```bash ./gen-posts.sh```
+3. Voilá. You have generated clutter-free blog 
--- gen-posts.sh
+++ gen-posts.sh
@@ -8,7 +8,7 @@ html_dir="./rendered"
 
 # Set the name of the index file
 index_file="index.html"
-
+template_path="templates/post.html"
 # Delete the existing index file if it exists
 rm -f "${html_dir}/${index_file}"
 
@@ -16,7 +16,7 @@ rm -f "${html_dir}/${index_file}"
 for md_file in "${md_dir}"/*.md; do
   # Convert the Markdown file to HTML
   html_file="${html_dir}/$(basename "$md_file" .md).html"
-  pandoc -f markdown -t html "${md_file}" -o "${html_file}"
+  pandoc -f markdown -t html "${md_file}" --template="${template_path}" -o "${html_file}"
 
   # Get the title of the Markdown file
   title=$(grep -E '^# ' "${md_file}" | sed -e 's/^# //')
