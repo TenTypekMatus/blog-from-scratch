3b4ef4e57ed07a362e49d432606f80e7ac10a8a7
Matus Mastena <Shadiness9530@proton.me>
2023-03-11 13:08:14 +0100

    Fixed bugs.

--- gen-posts.sh
+++ gen-posts.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 md_dir="./posts"
 html_dir="./rendered"
 index_file="index.html"
@@ -9,11 +8,4 @@ for md_file in "${md_dir}"/*.md; do
   # Convert the Markdown file to HTML
   html_file="${html_dir}/$(basename "$md_file" .md).html"
   pandoc -f markdown -t html "${md_file}" --template="${template_path}" -o "${html_file}"
-
-  title=$(grep -E '^# ' "${md_file}" | sed -e 's/^# //')
-
-  # Add the HTML file as an entry in the index file
-  echo "<li><a href=\"${html_file}/$(basename "$html_file")\">${title}</a></li>" >> "${html_dir}/temp.html"
 done
-sed -i '/<body>/r '${html_dir}/temp.html'' "./${index_file}"
-rm -f "${html_dir}/temp.html"
